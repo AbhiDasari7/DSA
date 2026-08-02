7955067ca576554b1879bba3e6a6531c4fce167c
AbhiDasari7 <abhiab77777@gmail.com>
2026-08-02 09:19:25 +0530

    Time: 1 ms (60.45%) | Memory: 44.2 MB (11.41%) - LeetSync

--- 4179-largest-even-number/largest-even-number.java
+++ 4179-largest-even-number/largest-even-number.java
@@ -3,7 +3,7 @@ class Solution {
         int l = s.length();
         for(int i = s.length()-1;i>=0;i--)
         {
-            if(s.charAt(i)=='0'||s.charAt(i)=='2'||s.charAt(i)=='4'||s.charAt(i)=='6'||s.charAt(i)=='8')
+            if(s.charAt(i)=='2')
             return s.substring(0,i+1);
         }
         return "";
