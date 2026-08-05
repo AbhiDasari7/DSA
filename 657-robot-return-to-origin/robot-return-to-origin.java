31a2aca36a5a137a8398ea6f3e32fadff01f4df3
AbhiDasari7 <abhiab77777@gmail.com>
2026-08-05 13:03:30 +0530

    Time: 8 ms (24.49%) | Memory: 45.4 MB (87.38%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int u = 0 , d=0 , l = 0 , r=0;
+        for(int i =0;i<moves.length();i++)
+        {
+            if(moves.charAt(i)=='U')
+            u++;
+            else if(moves.charAt(i)=='D')
+            d++;
+            else if(moves.charAt(i)=='L')
+            l++;
+            else
+            r++;
+        }
+        if(l==r&&u==d)
+        return true;
+        return false;
+        
+    }
+}
