e8332df5170d9ed93fb4a6c7feb1463d83eb85b6
AbhiDasari7 <abhiab77777@gmail.com>
2026-08-09 09:25:17 +0530

    Time: 17 ms (81.28%) | Memory: 49.8 MB (18.05%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int c =0;
         for(int i=0;i<nums.length;i+=2)
-        c=nums[i]<=nums[i+1]?c+nums[i]:c+nums[i+1];
+        c=c+nums[i];
         return c;
         
     }
