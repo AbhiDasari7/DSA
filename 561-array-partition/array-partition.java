0c4d31a65caea691f22784b284d76a7944285c10
AbhiDasari7 <abhiab77777@gmail.com>
2026-08-09 09:24:37 +0530

    Time: 18 ms (31.55%) | Memory: 49.8 MB (36.54%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,16 +1,10 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        List<Integer> a = new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-        a.add(nums[i]);
-        Collections.sort(a);
+        Arrays.sort(nums);
         int c =0;
-        for(int i =0;i<nums.length;i+=2)
-        {
-            c=a.get(i)<=a.get(i+1)?c+a.get(i):c+a.get(i+1);
-        }
+        for(int i=0;i<nums.length;i+=2)
+        c=nums[i]<=nums[i+1]?c+nums[i]:c+nums[i+1];
         return c;
-
         
     }
 }
